7fd1f147efd2af46b7a41e9ea8f6c50f21263690
Ilham Adiputra <job.ilhamadiputra4@gmail.com>
2021-09-24 11:44:37 +0700

    feat: read file in dir

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 
-import {View, Dimensions, Text} from 'react-native';
+import {View, Dimensions, Text, PermissionsAndroid} from 'react-native';
+import DocumentPicker from 'react-native-document-picker';
+import RNFS from 'react-native-fs';
 
 import Layout from '@components/layout';
 import Header from '@components/layout/header';
@@ -18,6 +20,53 @@ import AnimeList from '@components/organisms/home/animeList';
 
 const HomePage = ({navigation}) => {
   const [isAnimeOnList, setIsAnimeOnList] = useState(false);
+
+  const handlePickDirectory = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+      );
+
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        const getDirNameRegex = /%3A/g;
+        const response = await DocumentPicker.pickDirectory();
+
+        const externalDir = encodeURI(RNFS.ExternalStorageDirectoryPath + '/');
+        const selectedDir = response.uri.split(getDirNameRegex)[1];
+
+        const fullDirName = decodeURIComponent(externalDir + selectedDir);
+
+        // RNFS.readDir(fullDirName)
+        //   .then(result => {
+        //     console.log('GOT RESULT', result[0].name);
+        //     return Promise.all([RNFS.stat(result[0].path), result[0].path]);
+        //   })
+        //   .then(statResult => {
+        //     console.log(statResult);
+        //     if (statResult[0].isFile()) {
+        //       return RNFS.readFile(statResult[1], 'utf8');
+        //     }
+        //     return 'no file';
+        //   })
+        //   .then(contents => {
+        //     console.log(contents);
+        //   })
+        //   .catch(err => {
+        //     console.log(err.message, err.code);
+        //   });
+
+        const rd = await RNFS.readDir(fullDirName);
+
+        rd.forEach(read => {
+          if (read.isFile()) {
+            console.log(read.name);
+          }
+        });
+      }
+    } catch (error) {
+      console.log('err', error);
+    }
+  };
 
   return (
     <Layout>
@@ -57,7 +106,8 @@ const HomePage = ({navigation}) => {
               style={{borderRadius: 10, marginTop: 10}}
               onPress={() => {
                 console.log('tambah');
-                setIsAnimeOnList(true);
+                // setIsAnimeOnList(true);
+                handlePickDirectory();
               }}>
               Tambah Anime
             </CustomButton>
